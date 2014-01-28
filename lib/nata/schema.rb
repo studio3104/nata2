adbc92eac51bd7bfd472b70fed9abb0b97f17752
studio3104 <studio3104.com@gmail.com>
2014-01-28 21:40:29 +0900

    create table schema and verify being able to connect to mysqld

--- lib/nata/schema.rb
+++ lib/nata/schema.rb
@@ -1,89 +1,40 @@
 require 'mysql2-cs-bind'
+require 'yaml'
+
+class Hash
+  def symbolize_keys!
+    keys.each do |key|
+      self[(key.to_sym rescue key) || key] = delete(key)
+    end
+    self
+  end
+end
 
 module Nata
   class Schema
-    db_dir = File.join(File.dirname(__FILE__), '..', '..', 'db')
-    @db = case ENV['RACK_ENV']
-         when 'production'
-           SQLite3::Database.new(db_dir + '/production.db')
-         when 'test'
-           SQLite3::Database.new(db_dir + '/test.db')
-         else
-           SQLite3::Database.new(db_dir + '/development.db')
-         end
-    @db.execute('PRAGMA foreign_keys = ON')
-
-    def self.create_tables
-      @db.execute_batch <<-SQL
-        CREATE TABLE IF NOT EXISTS `hosts` (
-          `id`                 INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
-          `name`               VARCHAR(255) NOT NULL,
-          `created_at`         INTEGER NOT NULL DEFAULT (STRFTIME('%s', 'now', 'localtime')),
-          `updated_at`         INTEGER NOT NULL DEFAULT (STRFTIME('%s', 'now', 'localtime'))
-        );
-        CREATE UNIQUE INDEX IF NOT EXISTS `index_hosts_on_name` ON `hosts` (`name`);
-
-        CREATE TABLE IF NOT EXISTS `databases` (
-          `id`         INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
-          `host_id`    INTEGER NOT NULL,
-          `name`       VARCHAR(255) NOT NULL,
-          `rgb`        VARCHAR(255) NOT NULL DEFAULT '255,255,255',
-          `created_at` INTEGER NOT NULL DEFAULT (STRFTIME('%s', 'now', 'localtime')),
-          `updated_at` INTEGER NOT NULL DEFAULT (STRFTIME('%s', 'now', 'localtime')),
-          FOREIGN KEY (`host_id`) REFERENCES `hosts` (`id`)
-        );
-        CREATE INDEX IF NOT EXISTS `index_databases_on_host_id` ON `databases` (`host_id`);
-        CREATE UNIQUE INDEX IF NOT EXISTS `index_databases_on_host_id_and_name` ON `databases` (`host_id`, `name`);
-
-        CREATE TABLE IF NOT EXISTS `slow_queries` (
-          `id`            INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
-          `database_id`   INTEGER NOT NULL,
-          `date`          INTEGER NOT NULL, --unixtime
-          `user`          VARCHAR(255),
-          `host`          VARCHAR(255),
-          `query_time`    FLOAT NOT NULL DEFAULT 0.0,
-          `lock_time`     FLOAT NOT NULL DEFAULT 0.0,
-          `rows_sent`     INTEGER NOT NULL DEFAULT 0,
-          `rows_examined` INTEGER NOT NULL DEFAULT 0,
-          `sql`           VARCHAR(255) NOT NULL,
-          `created_at`    INTEGER NOT NULL DEFAULT (STRFTIME('%s', 'now', 'localtime')),
-          `updated_at`    INTEGER NOT NULL DEFAULT (STRFTIME('%s', 'now', 'localtime')),
-          FOREIGN KEY (`database_id`) REFERENCES `databases` (`id`)
-        );
-        CREATE INDEX IF NOT EXISTS `index_slow_queries_on_database_id` ON `slow_queries` (`database_id`);
-        CREATE INDEX IF NOT EXISTS `index_slow_queries_on_date` ON `slow_queries` (`date`);
-
-        CREATE TABLE IF NOT EXISTS `explains` (
-          `id`            INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
-          `slow_query_id` INTEGER,
-          `explain_id`    INTEGER,
-          `select_type`   VARCHAR(255),
-          `table`         VARCHAR(255),
-          `type`          VARCHAR(255),
-          `possible_keys` VARCHAR(255),
-          `key`           VARCHAR(255),
-          `key_len`       INTEGER,
-          `ref`           VARCHAR(255),
-          `rows`          INTEGER,
-          `extra`         VARCHAR(255),
-          `created_at`    INTEGER NOT NULL DEFAULT (STRFTIME('%s', 'now', 'localtime')),
-          `updated_at`    INTEGER NOT NULL DEFAULT (STRFTIME('%s', 'now', 'localtime')),
-          FOREIGN KEY (`slow_query_id`) REFERENCES `slow_queries` (`id`)
-        );
-        CREATE INDEX IF NOT EXISTS `index_explains_on_explain_id` ON `explains` (`slow_query_id`);
-      SQL
+    def self.client
+      @client ||= Mysql2::Client.new(
+        default_settings.merge(
+          {}
+#          YAML.load_file(ENV['NATA_DB_CONFIG']).symbolize_keys!
+        )
+      )
     end
 
-    def self.drop_all_tables
-      @db.execute_batch <<-SQL
-        DROP TABLE IF EXISTS `settings`;
-        DROP TABLE IF EXISTS `explains`;
-        DROP TABLE IF EXISTS `slow_queries`;
-        DROP TABLE IF EXISTS `databases`;
-        DROP TABLE IF EXISTS `hosts`;
-      SQL
+    def self.default_settings
+      {
+        host: '127.0.0.1',
+        username: 'root',
+        password: '',
+        database: ENV['RACK_ENV'] ? "nata_#{ENV['RACK_ENV']}" : 'nata_development',
+        port: 3306,
+        connect_timeout: 2
+      }
     end
   end
 end
-Nata::Schema.drop_all_tables
-Nata::Schema.create_tables
+
+
+
+cl = Nata::Schema.client
+p cl.query('show databases')
