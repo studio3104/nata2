375d0b95dd9ee1f1b4feca0f4a16de4c65f9d3e3
studio3104 <studio3104.com@gmail.com>
2014-04-25 15:52:16 +0900

    update to treat config

--- lib/nata2/config.rb
+++ lib/nata2/config.rb
@@ -6,8 +6,12 @@ CONFIG = TOML.load_file(File.expand_path('config.toml', "#{__dir__}/../.."))
 class Nata2::Config
   def self.get(keyword)
     case keyword
-    when :hrforecast_url
-      CONFIG[:hrforecast_url]
+    when :hffqdn
+      CONFIG['hrforecast']['fqdn']
+    when :hfport
+      CONFIG['hrforecast']['port']
+    when :hfhttps
+      CONFIG['hrforecast']['https']
     when :dburl
       CONFIG[:dburl] || 'sqlite://data/nata2.db'
     else
