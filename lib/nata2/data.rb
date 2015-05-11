aa31479a29f2efbf6e91e634e1c5f1ff35da3404
studio3104 <studio3104.com@gmail.com>
2015-05-11 19:16:19 +0900

    create graph more faster

--- lib/nata2/data.rb
+++ lib/nata2/data.rb
@@ -51,6 +51,34 @@ class Nata2::Data
     result.all
   end
 
+  # for graph data
+  def get_slow_queries_count_by_period(
+    id: nil, sort_by_date: false, limit: nil, offset: nil, 
+    from_datetime: 0, to_datetime: Time.now.to_i,
+    service_name: nil, host_name: nil, database_name: nil
+  )
+    bundles_where = { service_name: service_name, host_name: host_name, database_name: database_name }
+    bundles_where.delete_if { |k,v| v.nil? }
+    slow_queries_where = id ? { slow_queries__id: id } : {}
+
+    @bundles.where(bundles_where).left_outer_join(
+      :slow_queries, bundle_id: :id
+    ).where(
+      slow_queries_where
+    ).where {
+      (datetime >= from_datetime) & (datetime <= to_datetime)
+    }.select_group(
+      :bundles__service_name,
+      :bundles__host_name,
+      :bundles__database_name,
+      :slow_queries__period
+    ).select_append {
+      count(:slow_queries__period).as(:count)
+    }.reverse_order(
+      :datetime
+    ).limit(limit).offset(offset)
+  end
+
   def get_summarized_slow_queries(sort_order, slow_queries)
     Nata2::Mysqldumpslow.dump(slow_queries, sort_order)
   end
--- lib/nata2/helpers.rb
+++ lib/nata2/helpers.rb
@@ -40,45 +40,30 @@ module Nata2::Helpers
     end
   end
 
-  def graph_data(service_name, host_name, database_name, time_range)
+  def get_graph_data(service_name, host_name, database_name, time_range)
     from = from_datetime(time_range)
-    slow_queries = data.get_slow_queries(from_datetime: from, service_name: service_name, host_name: host_name, database_name: database_name)
+    graph_data = data.get_slow_queries_count_by_period(
+      from_datetime: from, service_name: service_name, host_name: host_name, database_name: database_name
+    )
+
+    #{"service_name":"service_name","host_name":"host_name2","database_name":"database_name","period":1431082800,"count":4}
+
+    graph_data = graph_data.to_a.group_by { |gd| gd[:period] }
+    result = []
+    graph_data.each do |period, gdata|
+      tgd = { period: Time.at(period).strftime('%Y-%m-%d %H:00') }
+      gdata.each do |gd|
+        tgd = tgd.merge({ %Q{#{gd[:database_name]}(#{gd[:host_name]})}.to_sym => gd[:count] })
+      end
+      result << tgd
+    end
+    return result
+
+    # TODO: plot zero if value is nothing
     plot_per, strftime_format = if time_range == 'y'
                                   [3600 * 24, '%Y-%m-%d']
                                 else
                                   [3600, '%Y-%m-%d %H:00']
                                 end
-
-    from_justified = 0
-    now = Time.now.to_i
-    (from..now).each do |time|
-      if time % plot_per == 0
-        from_justified = time
-        break
-      end
-    end
-
-    bundles = data.find_bundles(service_name: service_name, host_name: host_name, database_name: database_name)
-    db_names = bundles.map { |s| %Q{#{s[:database_name]}(#{s[:host_name]})} }
-    data = {}
-    db_names.each do |db_name|
-      __from = from_justified
-      while now >= __from
-        period = Time.at(__from).strftime(strftime_format)
-        __from += plot_per
-        data[period] ||= {}
-        data[period][db_name] = 0
-      end
-    end
-
-    slow_queries.each do |slow|
-      period = Time.at(slow[:datetime]).strftime(strftime_format)
-      db_name = %Q{#{slow[:database_name]}(#{slow[:host_name]})}
-      data[period] ||= {}
-      data[period][db_name] ||= 0
-      data[period][db_name] += 1
-    end
-
-    data.map { |period, count_of| { period: period }.merge(count_of) }
   end
 end
