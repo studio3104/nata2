4b0fea34309931042fe82cedf935897661436b9c
studio3104 <studio3104.com@gmail.com>
2015-05-12 15:03:32 +0900

    plot zero if value is nothing

--- lib/nata2/helpers.rb
+++ lib/nata2/helpers.rb
@@ -45,21 +45,31 @@ module Nata2::Helpers
     graph_data = data.get_slow_queries_count_by_period(
       from_datetime: from, service_name: service_name, host_name: host_name, database_name: database_name
     )
+    return {} if graph_data.empty?
 
     #{"service_name":"service_name","host_name":"host_name2","database_name":"database_name","period":1431082800,"count":4}
 
     graph_data = graph_data.to_a.group_by { |gd| gd[:period] }
+    temp = graph_data.max_by { |_, gd| gd.size }
+    template = {}
+    temp.last.each { |tmp|
+      template.merge!({
+        %Q{#{tmp[:database_name]}(#{tmp[:host_name]})}.to_sym => 0
+      })
+    }
     result = []
-    graph_data.each do |period, gdata|
-      tgd = { period: Time.at(period).strftime('%Y-%m-%d %H:00') }
-      gdata.each do |gd|
+    period = graph_data.min_by { |period, _| period }.first
+    max_period = graph_data.max_by { |period, _| period }.first
+    while period <= max_period do
+      tgd = template.merge(period: Time.at(period).strftime('%Y-%m-%d %H:00'))
+      graph_data[period].each do |gd|
         tgd = tgd.merge({ %Q{#{gd[:database_name]}(#{gd[:host_name]})}.to_sym => gd[:count] })
       end
       result << tgd
+      period += 3600
     end
     return result
 
-    # TODO: plot zero if value is nothing
     plot_per, strftime_format = if time_range == 'y'
                                   [3600 * 24, '%Y-%m-%d']
                                 else
